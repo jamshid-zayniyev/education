6f110273d0ceb5ea292393f14fb10fffab447203
jamshid-zayniyev <jamshidzayniev10@gmail.com>
2024-03-02 16:39:49 +0500

    Install pro

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      {/* logo  */}
+        <div>
+          <img src="" alt="" />
+        </div>
+
+      {/* nav-items    */}
+        <div>
+          <nav>
+            <ul>
+              <li><a href="#">Home</a></li>
+              <li><a href="#">About</a></li>
+              <li><a href="#">Blog</a></li>
+              <li><a href="#">Courses</a></li>
+              <li><a href="#">Contact</a></li>
+            </ul>
+          </nav>
+        </div>
+
+      {/* bar icons  */}
+        <div>
+          <span></span>
+          <span></span>
+        </div>
+
+    </div>
+  )
+}
+
+export default Navbar
